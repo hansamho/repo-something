82c2c695ba2783014d2444c2ea1a9929dd3f0c55
hansamho <tkagh258@naver.com>
2024-05-22 15:06:32 +0900

    aa

---
+++ something/src/main/java/com/something/infra/chat/ChatController.java
@@ -0,0 +1,15 @@
+package com.something.infra.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+
+	@RequestMapping(value="/chat")
+	public String chat()throws Exception{
+		
+		return "usr/infra/chat/chat";
+	}
+	
+}
---
+++ something/src/main/java/com/something/infra/chat/ChatDao.java
@@ -0,0 +1,5 @@
+package com.something.infra.chat;
+
+public interface ChatDao {
+
+}
